ed28100b71a30590fe42d9f72dd0e59f2fab27fb
= <yoann.robin.k@gmail.com>
2021-03-25 08:56:33 +0100

    Change license for GNU-GPL3

--- R/SBCK/R/bc.QMrs.R
+++ R/SBCK/R/bc.QMrs.R
@@ -1,86 +1,20 @@
 
-################################################################################
-################################################################################
-##                                                                            ##
-## Copyright Yoann Robin, 2019                                                ##
-##                                                                            ##
-## yoann.robin.k@gmail.com                                                    ##
-##                                                                            ##
-## This software is a computer program that is part of the SBCK (Statistical  ##
-## Bias Correction Kit). This library makes it possible to perform bias       ##
-## correction with non parametric methods, and give some metrics between      ##
-## Sparse Histogram is high dimensions.                                       ##
-##                                                                            ##
-## This software is governed by the CeCILL-C license under French law and     ##
-## abiding by the rules of distribution of free software.  You can  use,      ##
-## modify and/ or redistribute the software under the terms of the CeCILL-C   ##
-## license as circulated by CEA, CNRS and INRIA at the following URL          ##
-## "http://www.cecill.info".                                                  ##
-##                                                                            ##
-## As a counterpart to the access to the source code and  rights to copy,     ##
-## modify and redistribute granted by the license, users are provided only    ##
-## with a limited warranty  and the software's author,  the holder of the     ##
-## economic rights,  and the successive licensors  have only  limited         ##
-## liability.                                                                 ##
-##                                                                            ##
-## In this respect, the user's attention is drawn to the risks associated     ##
-## with loading,  using,  modifying and/or developing or reproducing the      ##
-## software by the user in light of its specific status of free software,     ##
-## that may mean  that it is complicated to manipulate,  and  that  also      ##
-## therefore means  that it is reserved for developers  and  experienced      ##
-## professionals having in-depth computer knowledge. Users are therefore      ##
-## encouraged to load and test the software's suitability as regards their    ##
-## requirements in conditions enabling the security of their systems and/or   ##
-## data to be ensured and,  more generally, to use and operate it in the      ##
-## same conditions as regards security.                                       ##
-##                                                                            ##
-## The fact that you are presently reading this means that you have had       ##
-## knowledge of the CeCILL-C license and that you accept its terms.           ##
-##                                                                            ##
-################################################################################
-################################################################################
-
-################################################################################
-################################################################################
-##                                                                            ##
-## Copyright Yoann Robin, 2019                                                ##
-##                                                                            ##
-## yoann.robin.k@gmail.com                                                    ##
-##                                                                            ##
-## Ce logiciel est un programme informatique faisant partie de la librairie   ##
-## SBCK (Statistical Bias Correction Kit). Cette librairie permet d'appliquer ##
-## une correction de biais avec des méthodes non paramétriques, et propose    ##
-## diverses metrique entre Histograme Sparse en haute dimension.              ##
-##                                                                            ##
-## Ce logiciel est régi par la licence CeCILL-C soumise au droit français et  ##
-## respectant les principes de diffusion des logiciels libres. Vous pouvez    ##
-## utiliser, modifier et/ou redistribuer ce programme sous les conditions     ##
-## de la licence CeCILL-C telle que diffusée par le CEA, le CNRS et l'INRIA   ##
-## sur le site "http://www.cecill.info".                                      ##
-##                                                                            ##
-## En contrepartie de l'accessibilité au code source et des droits de copie,  ##
-## de modification et de redistribution accordés par cette licence, il n'est  ##
-## offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,  ##
-## seule une responsabilité restreinte pèse sur l'auteur du programme, le     ##
-## titulaire des droits patrimoniaux et les concédants successifs.            ##
-##                                                                            ##
-## A cet égard  l'attention de l'utilisateur est attirée sur les risques      ##
-## associés au chargement,  à l'utilisation,  à la modification et/ou au      ##
-## développement et à la reproduction du logiciel par l'utilisateur étant     ##
-## donné sa spécificité de logiciel libre, qui peut le rendre complexe à      ##
-## manipuler et qui le réserve donc à des développeurs et des professionnels  ##
-## avertis possédant  des  connaissances  informatiques approfondies.  Les    ##
-## utilisateurs sont donc invités à charger  et  tester  l'adéquation  du     ##
-## logiciel à leurs besoins dans des conditions permettant d'assurer la       ##
-## sécurité de leurs systèmes et ou de leurs données et, plus généralement,   ##
-## à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.         ##
-##                                                                            ##
-## Le fait que vous puissiez accéder à cet en-tête signifie que vous avez     ##
-## pris connaissance de la licence CeCILL-C, et que vous en avez accepté les  ##
-## termes.                                                                    ##
-##                                                                            ##
-################################################################################
-################################################################################
+## Copyright(c) 2021 Yoann Robin
+## 
+## This file is part of SBCK.
+## 
+## SBCK is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## SBCK is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with SBCK.  If not, see <https://www.gnu.org/licenses/>.
 
 #' Quantile Mapping RankShuffle method
 #'
