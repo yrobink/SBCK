d9af0835530e2725206896f107d1683829231e76
Yoann Robin <yoann.robin.k@gmail.com>
2022-01-26 11:44:43 +0100

    version 0.4.3

--- python/SBCK/__release.py
+++ python/SBCK/__release.py
@@ -20,7 +20,7 @@
 version_major = 0
 version_minor = 4
 version_patch = 3
-version_extra = "a4"
+version_extra = ""
 version       = "{}.{}.{}{}".format(version_major,version_minor,version_patch,version_extra)
 
 name = "SBCK"
