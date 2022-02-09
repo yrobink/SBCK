622ff07750e8cc690523e2a7468e8ae40e9722d3
Yoann Robin <yoann.robin.k@gmail.com>
2022-02-09 17:27:03 +0100

    Patch 0.4.7

--- python/SBCK/__release.py
+++ python/SBCK/__release.py
@@ -20,7 +20,7 @@
 version_major = 0
 version_minor = 4
 version_patch = 7
-version_extra = "a0"
+version_extra = ""
 version       = "{}.{}.{}{}".format(version_major,version_minor,version_patch,version_extra)
 
 name = "SBCK"
